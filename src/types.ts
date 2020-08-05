23dfad70de534eb985ba1c76d02d59085461fba8
TaianeSB-94 <53235654+TaianeSB-94@users.noreply.github.com>
2020-08-05 14:10:24 -0300

    Primeiras modificações

---
+++ src/types.ts
@@ -0,0 +1,95 @@
+export const funcParams = 'data, theme, echartsInstance, echarts';
+
+const funcBody = `const series = data.series.map((s) => {
+  const sData = s.fields.find((f) => f.type === 'number').values.buffer;
+  const sTime = s.fields.find((f) => f.type === 'time').values.buffer;
+
+  return {
+    name: s.name,
+    type: 'bar',
+    showSymbol: false,
+    areaStyle: {
+      opacity: 0.1,
+    },
+    lineStyle: {
+      width: 1,
+    },
+    label: {
+      show: true,
+      position: 'top',
+    },
+    data: sData.map((d, i) => [sTime[i], d.toFixed(2)]),
+  };
+});
+
+const axisOption = {
+  axisTick: {
+    show: false,
+  },
+  axisLine: {
+    show: false,
+  },
+  axisLabel: {
+    color: 'rgba(128, 128, 128, .9)',
+  },
+  splitLine: {
+    lineStyle: {
+      color: 'rgba(128, 128, 128, .2)',
+    },
+  },
+};
+
+return {
+  backgroundColor: 'transparent',
+  tooltip: {
+    trigger: 'axis',
+  },
+  legend: {
+    type: 'scroll',
+    left: '0',
+    bottom: '0',
+    data: data.series.map((s) => s.name),
+    textStyle: {
+      color: 'rgba(128, 128, 128, .9)',
+    },
+  },
+  xAxis: Object.assign(
+    {
+      show: false,
+      type: 'time',
+    },
+    axisOption
+  ),
+  yAxis: Object.assign(
+    {
+      type: 'value',
+      min: '0',
+    },
+    axisOption
+  ),
+  grid: {
+    left: 0,
+    right: 16,
+    top: 6,
+    bottom: 24,
+    containLabel: true,
+  },
+  series,
+};`;
+
+// const getOption = `function (${funcParams}) {
+//   ${funcBody}
+// }`
+// const funcBodyReg = /{\n([\S\s]*)\n}/;
+// const matchResult = getOption.match(funcBodyReg);
+// const funcBody = matchResult ? matchResult[1] : '';
+
+export interface SimpleOptions {
+  followTheme: boolean,
+  getOption: string;
+}
+
+export const defaults: SimpleOptions = {
+  followTheme: false,
+  getOption: funcBody,
+};
